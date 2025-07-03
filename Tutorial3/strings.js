9250a83a5970756885c7ebab0e8d999d7f28c23c
Nikhil jain <nikhiljain90598@gmail.com>
2025-07-03 16:56:04 +0530

    Lecture 3

---
+++ Tutorial3/strings.js
@@ -0,0 +1,17 @@
+// Sequence of characters that is written inside the double quotes is known as strings 
+// Reverse of a string using loop in JS 
+let firstName = "Shraddha";
+let lastName = "Khapra";
+console.log(typeof(firstName));
+
+let MyName = ` I am 
+Nikhil 
+Jain `;
+
+console.log(MyName);
+
+// Operations on Strings 
+
+
+
+
